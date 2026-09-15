eb5ed7ac45327ce023a47c13ead0e3298596cb00
Khazin <175429319+KhazinAwan@users.noreply.github.com>
2026-09-15 21:38:04 +0500

    Create intial Home page and App

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+
+function App() {
+
+  return(
+    <>
+
+    <Navbar></Navbar>
+
+    <Home></Home>
+
+    <Footer></Footer>
+
+    </>
+
+  );
+
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+function Home() {
+
+    return (
+
+        <main>
+            <h1>Welcome to Mina-shop!</h1>
+            <p>Find what you need from our variety of products at great prices.</p>
+        </main>
+
+    );
+
+}
+
+export default Home;
