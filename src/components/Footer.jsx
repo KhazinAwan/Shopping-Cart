b7dbf4514be2b476e0515c6748194d7fb7bb8589
Khazin <175429319+KhazinAwan@users.noreply.github.com>
2026-09-15 18:05:02 +0500

    Create Navbar and Footer components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+function Footer() {
+
+    return(
+
+        <footer>
+            <p>
+                Made by Khazin, Sept 2026.
+            </p>
+        </footer>
+
+    );
+}
+
+export default Footer;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+function Navbar() {
+
+    return(
+
+        <nav>
+            <ul>
+                <li>
+                    <a href="/">Home</a>
+                </li>
+            </ul>
+
+            <ul>
+                <li>
+                    <a href="/shop">Shop</a>
+                </li>
+            </ul>
+
+            <ul>
+                <li>
+                    <a href="/cart">Cart (0)</a>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
