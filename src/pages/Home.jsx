e97654048824eb1515d11c16d0a5631ecbbd08cf
Khazin <175429319+KhazinAwan@users.noreply.github.com>
2026-09-24 17:02:41 +0500

    Add responsiveness to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,9 @@ function Home() {
 
     return (
 
-        <main className="flex flex-col flex-1 gap-4 justify-center text-center">
-            <h1 className="text-8xl font-bold text-amber-500">Welcome to Mina-shop!</h1>
-            <p className="text-4xl text-gray-700">Find what you need from our variety of products at great prices.</p>
+        <main className="flex flex-col flex-1 gap-4 justify-center text-center px-4">
+            <h1 className="text-5xl font-bold text-amber-500 sm:text-7xl lg:text-8xl">Welcome to Mina-shop!</h1>
+            <p className="text-2xl text-gray-700 sm:text-3xl lg:text-4xl">Find what you need from our variety of products at great prices.</p>
         </main>
 
     );
