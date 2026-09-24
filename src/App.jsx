05555455f9f82d09539ad3b7d631373cecf38a12
Khazin <175429319+KhazinAwan@users.noreply.github.com>
2026-09-24 16:05:56 +0500

    Add css to the Home page

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
       <Navbar />
 
-      <main className="flex-1">
+      <main className="flex flex-1">
 
         <Outlet />
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,9 @@ function Home() {
 
     return (
 
-        <main>
-            <h1>Welcome to Mina-shop!</h1>
-            <p>Find what you need from our variety of products at great prices.</p>
+        <main className="flex flex-col flex-1 gap-4 justify-center text-center">
+            <h1 className="text-8xl font-bold text-amber-500">Welcome to Mina-shop!</h1>
+            <p className="text-4xl text-gray-700">Find what you need from our variety of products at great prices.</p>
         </main>
 
     );
