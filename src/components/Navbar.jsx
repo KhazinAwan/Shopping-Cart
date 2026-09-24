bf1d6e272c1bc0075b9204dd937e78603300acab
Khazin <175429319+KhazinAwan@users.noreply.github.com>
2026-09-24 13:48:13 +0500

    Add a few improvements to Navbar and ProductCard components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,17 +11,17 @@ function Navbar() {
         <nav className="flex justify-between p-5 bg-gray-800 text-white">
             <ul>
                 <li>
-                    <Link to="/" className="hover:underline hover:decoration-amber-400 hover:underline-offset-4 focus:outline-none focus:ring-2 focus:ring-amber-400 focus:ring-offset-4 focus:ring-offset-gray-800">Home</Link>
+                    <Link to="/" className="hover:underline hover:decoration-amber-400 hover:underline-offset-4 focus:outline-none focus-visible:ring-2 focus-visible:ring-amber-400 focus-visible:ring-offset-4 focus-visible:ring-offset-gray-800">Home</Link>
                 </li>
             </ul>
 
             <ul className="flex gap-8">
                 <li>
-                    <Link to="/shop" className="hover:underline hover:decoration-amber-400 hover:underline-offset-4 focus:outline-none focus:ring-2 focus:ring-amber-400 focus:ring-offset-4 focus:ring-offset-gray-800">Shop</Link>
+                    <Link to="/shop" className="hover:underline hover:decoration-amber-400 hover:underline-offset-4 focus:outline-none focus-visible:ring-2 focus-visible:ring-amber-400 focus-visible:ring-offset-4 focus-visible:ring-offset-gray-800">Shop</Link>
                 </li>
 
                 <li>
-                    <Link to="/cart" className="hover:underline hover:decoration-amber-400 hover:underline-offset-4 focus:outline-none focus:ring-2 focus:ring-amber-400 focus:ring-offset-4 focus:ring-offset-gray-800">Cart {cart.reduce((total, product) => total + product.quantity, 0)}</Link>
+                    <Link to="/cart" className="hover:underline hover:decoration-amber-400 hover:underline-offset-4 focus:outline-none focus-visible:ring-2 focus-visible:ring-amber-400 focus-visible:ring-offset-4 focus-visible:ring-offset-gray-800">Cart {cart.reduce((total, product) => total + product.quantity, 0)}</Link>
                 </li>
             </ul>
         </nav>
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -65,7 +65,7 @@ return (
 
         </div>
 
-        <button type="button" className="rounded-md bg-amber-500 px-4 py-2 font-semibold text-white hover:bg-amber-600 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2" onClick={addToCart} >Add to Cart</button>
+        <button type="button" className="rounded-md bg-amber-500 px-4 py-2 font-semibold text-white hover:bg-amber-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-gray-600 focus-visible:ring-offset-2 active:scale-95 active:bg-amber-700" onClick={addToCart} >Add to Cart</button>
 
     </article>
 
