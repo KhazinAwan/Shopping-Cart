9f168424c4d6921057ac6c26c6f10fe33b261ea5
Khazin <175429319+KhazinAwan@users.noreply.github.com>
2026-09-24 12:12:10 +0500

    Add css to the Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,22 +8,20 @@ function Navbar() {
 
     return(
 
-        <nav>
+        <nav className="flex justify-between p-4 bg-gray-800 text-white">
             <ul>
                 <li>
-                    <Link to="/">Home</Link>
+                    <Link to="/" className="hover:underline hover:decoration-amber-400 hover:underline-offset-4 focus:outline-none focus:ring-2 focus:ring-amber-400 focus:ring-offset-4 focus:ring-offset-gray-800">Home</Link>
                 </li>
             </ul>
 
-            <ul>
+            <ul className="flex gap-8">
                 <li>
-                    <Link to="/shop">Shop</Link>
+                    <Link to="/shop" className="hover:underline hover:decoration-amber-400 hover:underline-offset-4 focus:outline-none focus:ring-2 focus:ring-amber-400 focus:ring-offset-4 focus:ring-offset-gray-800">Shop</Link>
                 </li>
-            </ul>
 
-            <ul>
                 <li>
-                    <Link to="/cart">Cart {cart.reduce((total, product) => total + product.quantity, 0)}</Link>
+                    <Link to="/cart" className="hover:underline hover:decoration-amber-400 hover:underline-offset-4 focus:outline-none focus:ring-2 focus:ring-amber-400 focus:ring-offset-4 focus:ring-offset-gray-800">Cart {cart.reduce((total, product) => total + product.quantity, 0)}</Link>
                 </li>
             </ul>
         </nav>
